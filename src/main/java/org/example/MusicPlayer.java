07aa17f80f3e78d426ab420c5c12154c74f6d809
max <limon.8bit@gmail.com>
2020-10-17 15:01:49 +0300

    inversion of control(IoC)

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MusicPlayer {
+     private Music music;
+
+     public  MusicPlayer(Music music){
+         this.music = music;
+     }
+
+     public void platMusic(){
+         System.out.printf("Play- " + music.getSong() );
+     }
+}
